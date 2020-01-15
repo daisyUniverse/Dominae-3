db1432a4e9c57b90e6904a2f21f37196981c7bbf
Evshaddock <evshaddock@gmail.com>
2020-01-15 16:54:54 -0600

    even more init, hopefully correctly this time

---
+++ Dominae 3/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Newtonsoft.Json;
+
+namespace Dominae_3
+{
+    internal class Program
+    {
+        private static Config _config; // this bit loads the token .json files
+        public Config LoadJson() 
+        {
+            using (StreamReader r = new StreamReader("config.json"))
+            {
+                string json = r.ReadToEnd();
+                List<Config> items = JsonConvert.DeserializeObject<List<Config>>(json);
+                return items[0];
+            }
+        }
+        public class Config
+        {
+            public string token;
+        }
+
+        private readonly DiscordSocketClient _client; // the motherfucker whomst runs discord
+        
+        public static void Main(string[] args)
+        {
+            new Program().MainAsync().GetAwaiter().GetResult();
+        }
+
+        public Program()
+        {
+            _client = new DiscordSocketClient();
+            Program._config = LoadJson();
+
+            _client.Log += LogAsync;
+            _client.Ready += ReadyAsync;
+            _client.MessageReceived += MessageRecievedAsync;
+        }
+
+        public async Task MainAsync()
+        {
+            await _client.LoginAsync(TokenType.Bot, _config.token);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task LogAsync(LogMessage log)
+        {
+            Console.WriteLine(log.ToString());
+            return Task.CompletedTask;
+        }
+
+        private Task ReadyAsync()
+        {
+            Console.WriteLine($"{_client.CurrentUser} is connected!");
+
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageRecievedAsync(SocketMessage message)
+        {
+            if (message.Author.Id == _client.CurrentUser.Id)
+                return;
+
+            if (message.Content == "live, fool")
+                await message.Channel.SendMessageAsync("No, you");
+        }
+    }
+}
